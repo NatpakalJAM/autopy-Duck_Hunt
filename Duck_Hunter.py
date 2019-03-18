aad115ef4204d4b66cdb64ce62c0bd99ecf3b550
Natpakal.k <natpakal.k@extreme.co.th>
2019-03-18 15:17:44 +0700

    update key terminate

--- Duck_Hunter.py
+++ Duck_Hunter.py
@@ -1,22 +1,35 @@
+#! python3
 import autopy
+import keyboard
 
 def findDuck(startArea):
     screen = autopy.bitmap.capture_screen()
-    pos = screen.find_color((255,119,99),0.03,((startArea[0], startArea[1]),(800,400)))
+    pos = screen.find_color((252, 116, 96), 0.03,
+                            ((startArea[0], startArea[1]), (800, 400)))
     if pos:
-        print("Found Duck at: %s" % str(pos))
-        autopy.mouse.move(pos[0]+20,pos[1])
+        # print("Found Duck at: %s" % str(pos))
+        autopy.mouse.move(pos[0]+20, pos[1])
         autopy.mouse.click()
+
 
 def findGameArea():
     for i in range(10):
         screen = autopy.bitmap.capture_screen()
-        pos = screen.find_color((0,204,255),0.03)
+        pos = screen.find_color((66, 198, 255), 0.03)
         if pos:
-            print("Game Area: " + str(pos))
+        #     print("Game Area: " + str(pos))
             return pos
     exit()
+
+
+def exitGame():
+    if keyboard.is_pressed('q'):
+        # print('Exit Duck AIM BOT !!')
+        return True
+    return False
 
 startArea = findGameArea()
 for i in range(1000):
     findDuck(startArea)
+    if exitGame() == True:
+        break
--- README.md
+++ README.md
@@ -10,6 +10,7 @@
     ```
 
 2) step 2: clone this bot
+
     ```
     $ git clone https://github.com/NatpakalJAM/autopy-Duck_Hunt.git
     ```
@@ -19,6 +20,7 @@
     link: [Duck_Hunt](https://www.borntodev.com/shoot-duck-game/)
 
 4) step 4: start game and bot
+
     ```
     $ python Duck_Hunter.py
     ```
@@ -26,3 +28,5 @@
 5) step 5: have fun
 
     :D
+
+    You can press `Q` for terminate this bot.
